78bc487a6e34e7612b379995b2ac54e4f54f96c1
kurone-kito <krone@kit.black>
2019-03-30 14:15:16 +0900

    Fixed a bug that fails under without privileges

--- setup.cmd
+++ setup.cmd
@@ -4,7 +4,7 @@ rem It execute recursive using UAC when without administrator privileges.
 whoami /groups | Find "High Mandatory Level" > NUL
 if not errorlevel 1 goto RUN
 echo You should elevate to administrator privileges, because continue setup.
-timeout /T /NOBREAK 3
+timeout /T 3 /NOBREAK
 powershell -Command Start-Process -Verb runas "%~0"
 exit /b %ERRORLEVEL%
 
