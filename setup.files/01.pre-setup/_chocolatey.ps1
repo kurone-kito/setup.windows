14987f789f3615973ec4879c99e904641b9f1045
kurone-kito <krone@kit.black>
2020-06-19 00:27:51 +0900

    fix: updated the installation of Chocolatey

--- setup.files/01.pre-setup/_chocolatey.ps1
+++ setup.files/01.pre-setup/_chocolatey.ps1
@@ -5,8 +5,8 @@ if (Get-Command choco -ea SilentlyContinue) {
 }
 else {
   New-Item -Path (Split-Path -Parent $profile) -ItemType directory -Force
-  New-Item -Path $profile -ItemType file
-  Set-ExecutionPolicy Bypass -Scope Process -Force;
+  New-Item -ErrorAction Ignore -Path $profile -ItemType file
+  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
   Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
   . $profile
