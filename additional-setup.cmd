b715342ff5a4b444a6aa2d4659765b822e798a03
kurone-kito <krone@kit.black>
2022-07-18 13:56:07 +0900

    fix: fixed a bug that cannot launch the additional setup scripts

--- additional-setup.cmd
+++ additional-setup.cmd
@@ -1,7 +1,1 @@
-@echo off
-
-echo Welcome to the setup.windows!
-echo Preparing...
-
-powershell -Command "Unblock-File %~dp0pre\index.ps1"
-powershell -NoProfile -ExecutionPolicy Bypass %~dp0pre\index.ps1
+@powershell -NoProfile -ExecutionPolicy Bypass %~dp0libs\additional-setup.ps1
